3af489426fd9e4f6e5b92c23bcab2c77e7348aea
Taymoor <61752890+taymoorkhan@users.noreply.github.com>
2020-11-23 12:36:24 -0800

    Update authController
    registerSubmit has a cookie session added, however it is not running for some reason

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -1,5 +1,7 @@
 let database = require("../database");
 
+
+
 let authController = {
   login: (req, res) => {
     res.locals.path = req.path
@@ -7,17 +9,27 @@ let authController = {
   },
 
   register: (req, res) => {
+    // console.log("ok")
     res.locals.path = req.path
     let email = req.query.email
     res.render('auth/register',{email: email})
   },
 
   loginSubmit: (req, res) => {
-    // implement
+    //implement
   },
 
   registerSubmit: (req, res) => {
-    // implement
+//     console.log("test")
+    if (req.body.email && req.body.password){
+      database[req.body.email] = {email: req.body.email, password: req.body.password};
+      req.session["email"] = req.body.email;
+      res.redirect('/reminders')
+    } else{
+//       console.log("err")
+      res.status(400);
+      res.send("invalid user")
+    }
   }
 }
 
